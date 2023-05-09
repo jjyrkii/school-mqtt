b27b33968bbb5d8bd17d01c81a81a06cbd65ce8a
jyrki <jjyrkii@protonmail.com>
2023-05-09 20:07:29 +0200

    added constructor and timestamps to message

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 // Message is a struct to hold a message.
 type Message struct {
-	Message string `json:"message" binding:"required"`
+	Message string    `json:"message" binding:"required"`
+	Time    time.Time `json:"timestamp"`
+}
+
+func NewMessage(message string) Message {
+	return Message{
+		Message: message,
+		Time:    time.Now(),
+	}
 }
 
 // MessageCollection is a struct to hold a collection of messages.
@@ -41,9 +50,9 @@ func main() {
 
 // GetPong returns a simple message.
 func GetPong(c *gin.Context) {
-	message := Message{"pong"}
+	m := NewMessage("pong")
 	c.JSON(http.StatusOK, gin.H{
-		"body": message,
+		"body": m,
 	})
 }
 
@@ -51,8 +60,8 @@ func GetPong(c *gin.Context) {
 // Returns a 200 status code if successful.
 // Returns a 400 status code if the message is missing or not a string.
 func AddMessage(c *gin.Context) {
-	var json Message
-	err := c.ShouldBindJSON(&json)
+	m := NewMessage("")
+	err := c.ShouldBindJSON(&m)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Message is required and must be a string.",
@@ -60,7 +69,7 @@ func AddMessage(c *gin.Context) {
 		return
 	}
 
-	collection.Messages = append(collection.Messages, json)
+	collection.Messages = append(collection.Messages, m)
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Message successfully added.",
