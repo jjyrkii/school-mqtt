da3452a99202f8f0c2b3d457567b81c2a2817cfe
jyrki <jjyrkii@protonmail.com>
2023-05-09 18:51:35 +0200

    added first handler function for routes and message struct

--- main.go
+++ main.go
@@ -7,16 +7,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Message is a struct to hold a message.
+type Message struct {
+	Message string `json:"message"`
+}
+
 // main is the entry point for the application.
 func main() {
+
+	// Server settings
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// Define the routes for the application.
+	r.GET("/ping", GetPong)
+
+	// Start serving the application.
 	err := r.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// GetPong returns a simple message.
+func GetPong(c *gin.Context) {
+	message := Message{"pong"}
+	c.JSON(http.StatusOK, gin.H{
+		"message": message.Message,
+	})
 }
