f99d877dc411262f6cefe40f4c7bd44e12fb7519
jyrki <jjyrkii@protonmail.com>
2023-05-09 18:43:18 +0200

    added gin

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+// main is the entry point for the application.
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
