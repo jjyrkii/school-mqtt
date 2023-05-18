f205838f5488a1ab58cbe366f8eeaef17f863a5d
jyrki <jjyrkii@protonmail.com>
2023-05-18 19:22:15 +0200

    moved everything to main.go and added mqtt subscriber

--- main.go
+++ main.go
@@ -1,87 +1,129 @@
 package main
 
-//package main
-//
-//import (
-//	"log"
-//	"net/http"
-//	"time"
-//
-//	"github.com/gin-gonic/gin"
-//)
-//
-//// Message is a struct to hold a message.
-//type Message struct {
-//	Message string    `json:"message" binding:"required"`
-//	Time    time.Time `json:"timestamp"`
-//}
-//
-//// NewMessage is the constructor for the Message struct.
-//func NewMessage(message string) Message {
-//	return Message{
-//		Message: message,
-//		Time:    time.Now(),
-//	}
-//}
-//
-//// MessageCollection is a struct to hold a collection of messages.
-//type MessageCollection struct {
-//	Messages []Message `json:"messages"`
-//}
-//
-//// collection is a global variable to hold the messages.
-//// This is a placeholder for a database.
-//var collection MessageCollection
-//
-//// main is the entry point for the application.
-//func main() {
-//
-//	// Server settings
-//	r := gin.Default()
-//
-//	// Define the routes for the application.
-//	r.GET("/ping", GetPong)
-//	r.GET("/messages", GetMessages)
-//	r.POST("/messages", AddMessage)
-//
-//	// Start serving the application.
-//	err := r.Run()
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//}
-//
-//// GetPong returns a simple message.
-//func GetPong(c *gin.Context) {
-//	m := NewMessage("pong")
-//	c.JSON(http.StatusOK, gin.H{
-//		"body": m,
-//	})
-//}
-//
-//// AddMessage adds a message to the collection.
-//// Returns a 200 status code if successful.
-//// Returns a 400 status code if the message is missing or not a string.
-//func AddMessage(c *gin.Context) {
-//	m := NewMessage("")
-//	err := c.ShouldBindJSON(&m)
-//	if err != nil {
-//		c.JSON(http.StatusBadRequest, gin.H{
-//			"message": "Message is required and must be a string.",
-//		})
-//		return
-//	}
-//
-//	collection.Messages = append(collection.Messages, m)
-//
-//	c.JSON(http.StatusOK, gin.H{
-//		"message": "Message successfully added.",
-//	})
-//}
-//
-//// GetMessages returns a list of messages.
-//func GetMessages(c *gin.Context) {
-//	c.JSON(http.StatusOK, gin.H{
-//		"data": collection,
-//	})
-//}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/gin-gonic/gin"
+)
+
+// Message is a struct to hold a message.
+type Message struct {
+	ID      int       `json:"id"`
+	Message string    `json:"message" binding:"required"`
+	Time    time.Time `json:"timestamp"`
+}
+
+// NewMessage is the constructor for the Message struct.
+func NewMessage(message string) Message {
+	return Message{
+		ID:      len(collection) + 1,
+		Message: message,
+		Time:    time.Now(),
+	}
+}
+
+// collection is a global variable to hold the messages.
+// This is a placeholder for a database.
+var collection []Message
+
+// client is a global variable to hold the MQTT client.
+var client mqtt.Client
+
+// main is the entry point for the application.
+func main() {
+	// Create a new MQTT client.
+	buildClient()
+
+	// Subscribe to the topic.
+	// The callback function is called when a message is received.
+	// The message is added to the collection.
+	client.Subscribe("topic/test", 0, func(client mqtt.Client, msg mqtt.Message) {
+		collection = append(collection, NewMessage(string(msg.Payload())))
+	})
+
+	// Set up the HTTP server.
+	gin.SetMode(gin.ReleaseMode)
+	server := gin.Default()
+
+	// Define the routes for the application.
+	server.GET("/messages", GetMessages)
+	server.POST("/messages", AddMessage)
+
+	// Start serving the application.
+	err := server.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// AddMessage adds a message to the collection.
+// Returns a 200 status code if successful.
+// Returns a 400 status code if the message is missing or not a string.
+func AddMessage(c *gin.Context) {
+	m := NewMessage("")
+	err := c.ShouldBindJSON(&m)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Message is required and must be a string.",
+		})
+		return
+	}
+
+	collection = append(collection, m)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Message successfully added.",
+	})
+}
+
+// GetMessages returns a list of all collected messages.
+func GetMessages(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"data": collection,
+	})
+}
+
+// buildClient creates a new MQTT client.
+func buildClient() {
+	// broker string
+	var broker = "ee58e6440f874431835beb51cc1fbd50.s2.eu.hivemq.cloud"
+
+	// port number
+	var port = 8883
+
+	// define the options for the client
+	opts := mqtt.NewClientOptions()
+
+	// set the connection options
+	opts.AddBroker(fmt.Sprintf("tls://%s:%d", broker, port))
+	opts.SetClientID("<client_name>") // set a name as you desire
+	opts.SetUsername("username")      // these are the credentials that you declare for your cluster
+	opts.SetPassword("Password123")
+
+	// set callback handlers that get called on certain events
+	//opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+
+	// create a new client using the options
+	client = mqtt.NewClient(opts)
+
+	// handle possible errors on connection
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+}
+
+// upon connection to the client, this is called
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected to client")
+}
+
+// this is called when the connection to the client is lost, it prints "Connection lost" and the corresponding error
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connection lost: %v", err)
+}
